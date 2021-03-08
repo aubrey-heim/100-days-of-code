c9fd6e9eb826e5d243411a274ff9fc7b2602918f
aubrey-heim <aubrey.l.heim@gmail.com>
2021-03-08 12:28:57 -0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
+import { Container } from "@material-ui/core";
+import Footer from "./components/Footer"
+import Header from "./components/Header";
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Container>
+          <Switch>
+            <Route exact path={["/", "/home"]}>
+              <Home />
+            </Route>
+            <Route>
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Container>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;

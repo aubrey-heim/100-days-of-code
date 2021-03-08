89f7b80df5235f1ab19b16a02b08ca8b61b1c4c9
aubrey-heim <aubrey.l.heim@gmail.com>
2021-03-08 12:12:18 -0800

    set up API calls

---
+++ client/src/utils/API.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export default {
+  // Gets all recaps
+  getRecaps: function () {
+    return axios.get("/api/recaps/");
+  },
+  // Gets the recap with the given id
+  getRecap: function (id, uid) {
+    return axios.get("/api/recaps/" + id);
+  },
+  // Deletes the recap with the given id
+  deleteRecap: function (id) {
+    return axios.delete("/api/recaps/" + id);
+  },
+  // Saves a recap to the database
+  saveRecap: function (recapData) {
+    return axios.post("/api/recaps", recapData);
+  },
+  updateRecap: function (id, recapData) {
+    return axios.put("/api/recaps/" + id, recapData);
+  },
+};

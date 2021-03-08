01cbe651b7e973efbdd121773a873a6320b99e0c
aubrey-heim <aubrey.l.heim@gmail.com>
2021-03-08 12:11:45 -0800

    set up nomatch page

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,17 @@
+//import react
+import React from "react";
+//creates page which displays when the path is not found
+function NoMatch() {
+  return (
+    <div >
+      <h1>404 Page Not Found</h1>
+      <h1>
+        <span role="img" aria-label="Face With Rolling Eyes Emoji">
+          🤷
+        </span>
+      </h1>
+    </div>
+  );
+}
+//exports the no match page
+export default NoMatch;
